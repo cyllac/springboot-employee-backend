f90da0a545b954dc8d3184f60a890c7d9c440742
Priscila <cyllac@gmail.com>
2022-09-18 10:45:38 -0300

    Spring boot employee project. First commit.

---
+++ src/main/java/com/cyllac/springbootemployeebackend/SpringbootEmployeeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cyllac.springbootemployeebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmployeeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEmployeeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyllac/springbootemployeebackend/SpringbootEmployeeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyllac.springbootemployeebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEmployeeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
